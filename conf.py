641ffa18ecfecd42415f5233f389803cde0fd738
alek1984 <154059778+alek1984@users.noreply.github.com>
2025-03-16 16:52:04 +0200

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,4 +1,11 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
+sys.path.insert(0, os.path.abspath('../../'))  # Додаємо шлях до проєкту
+
+extensions = [
+    'sphinx.ext.autodoc',          # Автоматичне генерування документації з docstrings
+    'sphinx.ext.napoleon',         # Підтримка Google та NumPy стилю
+    'sphinx.ext.viewcode',         # Додає посилання на вихідний код
+    'sphinx_autodoc_typehints'     # Додає анотації типів у документацію
+]
+
